b47aa979602a048365e6726c6ffcba2dc3ca1b50
Emmanuel Chucks <echucks19@gmail.com>
2022-10-19 04:00:05 +0000

    refactor: use solid-icons for icons
    - render issue fixed from library author

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 
-import image from '@astrojs/image'
+// https://astro.build/config
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
-  ],
-})
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), solidJs()]
+});
