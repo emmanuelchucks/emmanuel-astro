5c340d727bb82e79bcb8b883a7ed546f968fdc77
Emmanuel Chucks <echucks19@gmail.com>
2022-12-03 02:06:55 +0000

    use prettier via plugin
    - fix formatting bugs in imports
    - special config for pnpm support
    - more consistency with tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), solidJs()]
-});
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    solidJs(),
+  ],
+})
