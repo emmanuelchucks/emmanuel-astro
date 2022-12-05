11ad47f948a47d4ad155f7d701def1ec52b6d3ff
Emmanuel Chucks <echucks19@gmail.com>
2022-12-05 20:22:35 +0000

    create and use PostCard for blog post listings

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,11 @@
 export interface Frontmatter {
   title: string
-  description: string
   author: string
   pubDate: string
   minutesRead: string
   tags: string[]
   image: {
-    url: string
+    src: string
     alt: string
   }
 }
